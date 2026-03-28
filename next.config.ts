e609b5c579b27e3c3be596add8e1b9e746144c87
DuyKhuong <khuongtran227@gmail.com>
2026-03-28 17:07:26 +0700

    feat: add additional remote image patterns to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,46 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.hsv-tech.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'file.hstatic.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.nhathuoclongchau.com.vn',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.hasaki.vn',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ordinary.com.vn',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pinterest.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.cosmetics.vn',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.pinterest.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.hcdn.vn',
+      },
     ],
   },
 
